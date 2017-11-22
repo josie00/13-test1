f4ddd7673f405b1a6d36f2519797911a19ef9ecb
Josie00 <xl4tn@virginia.edu>
2017-11-22 14:01:42 -0500

    customer and employee beans

---
+++ OBS/src/main/java/com/obs/databean/Employee.java
@@ -0,0 +1,44 @@
+package com.obs.databean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long employeeId;
+    private String firstName;
+    private String lastName;
+    private String title;
+    private String userName;
+    private String password;
+    private String email;
+    
+    protected Employee() {}
+
+	public Employee(Long employeeId, String firstName, String lastName, String title, String userName, String password,
+			String email) {
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.title = title;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName + ", title="
+				+ title + ", userName=" + userName + ", password=" + password + ", email=" + email + "]";
+	}
+	
+	
+    
+    
+	
+}
