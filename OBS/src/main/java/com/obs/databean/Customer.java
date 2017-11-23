fc01fe7bb46dfc4228a9b51f2fc5a232521773f7
Josie00 <xl4tn@virginia.edu>
2017-11-23 16:30:45 -0500

    customer

--- OBS/src/main/java/com/obs/databean/Customer.java
+++ OBS/src/main/java/com/obs/databean/Customer.java
@@ -57,13 +57,129 @@ public class Customer {
 		this.driverLicense = driverLicense;
 		this.accounts = accounts;
 	}
+	
+	
 
-	@Override
-	public String toString() {
-		return "Customer [customerId=" + customerId + ", userName=" + userName + ", email=" + email + ", password="
-				+ password + ", firstName=" + firstName + ", lastName=" + lastName + ", street=" + street + ", city="
-				+ city + ", state=" + state + ", zip=" + zip + ", phone=" + phone + ", dateOfBirth=" + dateOfBirth
-				+ ", ssn=" + ssn + ", driverLicense=" + driverLicense + ", accounts=" + accounts + "]";
+	public long getCustomerId() {
+		return customerId;
 	}
+
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getDriverLicense() {
+		return driverLicense;
+	}
+
+	public void setDriverLicense(String driverLicense) {
+		this.driverLicense = driverLicense;
+	}
+
+	public Set<Account> getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(Set<Account> accounts) {
+		this.accounts = accounts;
+	}
+
+	
     
 }
--- OBS/src/main/java/com/obs/repository/EmployeeRepository.java
+++ OBS/src/main/java/com/obs/repository/EmployeeRepository.java
@@ -3,8 +3,9 @@ package com.obs.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.obs.databean.Account;
+import com.obs.databean.Employee;
 
-public interface EmployeeRepository extends CrudRepository<Account, Long> {
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 
 }
