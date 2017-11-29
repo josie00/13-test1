2d445dda684ab2d21881bf8096de6e4d9142e9e8
Josie00 <xl4tn@virginia.edu>
2017-11-29 15:30:30 -0500

    add account number

--- OBS/src/main/java/com/obs/databean/Loan.java
+++ OBS/src/main/java/com/obs/databean/Loan.java
@@ -24,6 +24,7 @@ public class Loan {
 	private String status;
 	private double duePayment;
 	private double totalAmount;
+	private String loanNumber;
 	
 	@ManyToOne(cascade= CascadeType.ALL)
     @JoinColumn(name = "loan_type_id")
@@ -40,6 +41,106 @@ public class Loan {
 	private Set<Transaction> transactions;
 	
 	protected Loan() {}
+
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+
+	public Date getNextDueDate() {
+		return nextDueDate;
+	}
+
+
+	public void setNextDueDate(Date nextDueDate) {
+		this.nextDueDate = nextDueDate;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public double getDuePayment() {
+		return duePayment;
+	}
+
+
+	public void setDuePayment(double duePayment) {
+		this.duePayment = duePayment;
+	}
+
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+
+	public String getLoanNumber() {
+		return loanNumber;
+	}
+
+
+	public void setLoanNumber(String loanNumber) {
+		this.loanNumber = loanNumber;
+	}
+
+
+	public LoanType getLoanType() {
+		return loanType;
+	}
+
+
+	public void setLoanType(LoanType loanType) {
+		this.loanType = loanType;
+	}
+
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+
+	public Set<Transaction> getTransactions() {
+		return transactions;
+	}
+
+
+	public void setTransactions(Set<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Loan [loanId=" + loanId + ", startDate=" + startDate + ", nextDueDate=" + nextDueDate + ", status="
+				+ status + ", duePayment=" + duePayment + ", totalAmount=" + totalAmount + ", loanNumber=" + loanNumber
+				+ ", loanType=" + loanType + ", customer=" + customer + ", transactions=" + transactions + "]";
+	}
+	
+	
 
 	
 }
