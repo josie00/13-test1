05af08ee02935cb9776d069a964f104308b8d842
ziyahahah <ziyaowu.i@gmail.com>
2018-02-11 15:24:03 -0500

    transition day and create fund

--- CFS/src/main/java/com/cfs/api/EmployeeApiController.java
+++ CFS/src/main/java/com/cfs/api/EmployeeApiController.java
@@ -1,7 +1,9 @@
 package com.cfs.api;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -14,7 +16,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cfs.databean.Fund;
+import com.cfs.databean.FundPriceHistory;
 import com.cfs.repository.EmployeeRepository;
+import com.cfs.repository.FundPriceHistoryRepository;
 import com.cfs.repository.FundRepository;
 
 @RestController
@@ -26,8 +30,11 @@ public class EmployeeApiController {
 	@Autowired
 	EmployeeRepository er;
 	
+	@Autowired
+	FundPriceHistoryRepository fphr;
+	
 	@RequestMapping(value = "/createFund", method = RequestMethod.POST)
-	public @ResponseBody Map<String, String> login(@RequestBody Map<String, String> map, HttpServletRequest request) {
+	public @ResponseBody Map<String, String> createFund(@RequestBody Map<String, String> map, HttpServletRequest request) {
 		HttpSession session = request.getSession();
 		Map<String, String> res = new HashMap<String,String>();
 
@@ -47,10 +54,13 @@ public class EmployeeApiController {
 		String symbol = map.get("symbol");
 		String initial_value = map.get("initial_value");
 		
-		// what message to send if failed parse?
+		// what message to send if failed parse / less than 0?
 		double value = 0;
 		try {
 			value = Double.parseDouble(initial_value);
+			if (value < 0.005) {
+				res.put("message", "????");
+			}
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		}
@@ -59,6 +69,38 @@ public class EmployeeApiController {
 		fr.save(fund);
 		res.put("message", "The fund was successfully created");
 		
+		return res;
+	}
+	
+	@RequestMapping(value = "/transitionDay", method = RequestMethod.POST)
+	public @ResponseBody Map<String, String> transitionDay(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Map<String, String> res = new HashMap<String,String>();
+
+		// Check session
+		if (session.getAttribute("type") == null) {
+			res.put("message", "You are not currently logged in");
+			return res;
+		}
+		
+		// Check if employee
+		if (! ((String) session.getAttribute("type")).equals("employee")) {
+			res.put("message", "You must be an employee to perform this action");
+			return res;
+		}
+		
+		// Update fund price
+		Iterable<Fund> funds = fr.findAll();
+		for (Fund fund : funds) {
+			double oldPrice = fund.getCurrPrice();
+			double newPrice = ThreadLocalRandom.current().nextDouble(oldPrice * 0.9, oldPrice * 1.1);
+			while (newPrice < 0.005) {
+				newPrice = ThreadLocalRandom.current().nextDouble(oldPrice * 0.9, oldPrice * 1.1);
+			}
+			fund.setCurrPrice(newPrice);
+			fr.save(fund);
+		}
+		res.put("message", "The fund prices have been successfully recalculated");
 		return res;
 	}
 }
