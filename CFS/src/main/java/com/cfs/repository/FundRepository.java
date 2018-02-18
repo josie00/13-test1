8fb9ec62bfce958cfab85c3a69833d1755f274aa
Josie00 <xl4tn@virginia.edu>
2018-02-18 11:52:46 -0500

    unlock

--- CFS/src/main/java/com/cfs/repository/FundRepository.java
+++ CFS/src/main/java/com/cfs/repository/FundRepository.java
@@ -13,15 +13,15 @@ import org.springframework.data.repository.CrudRepository;
 import com.cfs.databean.Fund;
 import java.lang.String;
 
-@Transactional
+//@Transactional
 public interface FundRepository extends CrudRepository<Fund, Long> {
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
+//	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	List<Fund> findBySymbol(String symbol);
 	
-	@Lock(LockModeType.PESSIMISTIC_WRITE)
+//	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	List<Fund> findBySymbolAndName(String symbol, String name);
 	
-	@Transactional
+//	@Transactional
 	@Modifying
 //	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("update Fund f set f.currPrice = ?1 where f.fundId = ?2")
